c9fc209f5f4bc74539e995c232a2bd59491143f4
shaunik3006 <svir0013@student.monash.edu>
2026-05-26 16:46:25 +1000

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ vegaEmbed('#chart4', 'charts/chart4.json', embedOpts);
 vegaEmbed('#chart5', 'charts/chart5.json', embedOpts);
 vegaEmbed('#chart6', 'charts/chart6.json', embedOpts);
 vegaEmbed('#chart7', 'charts/chart7.json', embedOpts);
+vegaEmbed('#chart8', 'charts/chart8.json', embedOpts);
 vegaEmbed('#chart9', 'charts/chart9.json', embedOpts);
 vegaEmbed('#chart10', 'charts/chart10.json', embedOpts);
 vegaEmbed('#chart11', 'charts/chart11.json', embedOpts);
