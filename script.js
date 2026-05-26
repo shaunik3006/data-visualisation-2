b99ea1422a0a8d1e4d32b0c607478ac730ea54b3
shaunik123 <shaunikv123@gmail.com>
2026-05-26 16:32:29 +1000

    chart blut issue

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-vegaEmbed('#chart1', 'charts/chart1.json', { actions: false })
-vegaEmbed('#chart2', 'charts/chart2.json', { actions: false })
-vegaEmbed('#chart3', 'charts/chart3.json', { actions: false })
-vegaEmbed('#chart4', 'charts/chart4.json', { actions: false })
-vegaEmbed('#chart5', 'charts/chart5.json', { actions: false })
-vegaEmbed('#chart6', 'charts/chart6.json', { actions: false })
-vegaEmbed('#chart7', 'charts/chart7.json', { actions: false })
-vegaEmbed('#chart8', 'charts/chart8.json', { actions: false })
-vegaEmbed('#chart9', 'charts/chart9.json', { actions: false })
-vegaEmbed('#chart10', 'charts/chart10.json', { actions: false })
-vegaEmbed('#chart11', 'charts/chart11.json', { actions: false })
-vegaEmbed('#chart12', 'charts/chart12.json', { actions: false })
-vegaEmbed('#chart13', 'charts/chart13.json', { actions: false });
+const embedOpts = { actions: false, scaleFactor: 2 };
+
+vegaEmbed('#chart1', 'charts/chart1.json', embedOpts);
+vegaEmbed('#chart2', 'charts/chart2.json', embedOpts);
+vegaEmbed('#chart3', 'charts/chart3.json', embedOpts);
+vegaEmbed('#chart4', 'charts/chart4.json', embedOpts);
+vegaEmbed('#chart5', 'charts/chart5.json', embedOpts);
+vegaEmbed('#chart6', 'charts/chart6.json', embedOpts);
+vegaEmbed('#chart7', 'charts/chart7.json', embedOpts);
+vegaEmbed('#chart8', 'charts/chart8.json', embedOpts);
+vegaEmbed('#chart9', 'charts/chart9.json', embedOpts);
+vegaEmbed('#chart10', 'charts/chart10.json', embedOpts);
+vegaEmbed('#chart11', 'charts/chart11.json', embedOpts);
+vegaEmbed('#chart12', 'charts/chart12.json', embedOpts);
+vegaEmbed('#chart13', 'charts/chart13.json', embedOpts);
